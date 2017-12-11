2ccd8df5be541b4cc49dfd0179024a954aeb4458
mortuz <30647727+mortuz@users.noreply.github.com>
2017-12-11 12:15:17 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,91 @@
 (function($){
-    var data = [];
+    var selList = [];
+
+    var banquet = [];
+    var administration = [];
+    
+    var currentList;
+    var currentHook;
+
+    if(!currentList) {
+        $('.list').hide();
+    }
+    // populate list
+    var checkboxes = $('.checkbox');
+    for (let i = 0; i < checkboxes.length; i++) {
+        const checkbox = checkboxes[i];
+        var val = checkbox.parentNode.innerText;
+
+        selList.push(val);
+    }
+
+
+    // Change current list
+    var changecurrentList = function(list) {
+        $('.list').show();
+        currentList = list;
+        unceckAllCheckboxes();
+        checkSelectedCheckboxes(list);
+    }
+
+    var checkSelectedCheckboxes = function(list) {
+        var checkboxes = $('.checkbox');
+        for (let i = 0; i < checkboxes.length; i++) {
+            const checkbox = checkboxes[i];
+            var val = checkbox.parentNode.innerText;
+
+            var index = list.indexOf(val);
+            console.log(index)
+            if(index > -1) {
+                checkbox.checked = true;
+            }
+        }
+    }
+
+    var unceckAllCheckboxes = function() {
+        var checkboxes = $('.checkbox');
+        checkboxes.prop('checked', false)
+    }
+    
+    var changeCurrentHook = function(hook) {
+        currentHook = hook;
+    }
+
+    $('.input').click(function(){
+        var variable = $(this).attr('data-var');
+        var hook = $(this).find('.hook');
+        $('.current-heading').text($(this).prev().prev().text())
+        changeCurrentHook(hook);
+        
+        if(variable=='banquet') {
+            changecurrentList(banquet);
+        } else if (variable == 'administration') {
+            changecurrentList(administration);
+        }
+    })
 
     $('.checkbox').change(function(e){
         var selectedValue = ($(this).parent()[0]).innerText;
         if($(this).is(':checked')) {
-            data.push(selectedValue)
+            currentList.push(selectedValue)
         } else {
-            var index = data.indexOf(selectedValue)
-            data.splice(index, 1);
+            var index = currentList.indexOf(selectedValue)
+            currentList.splice(index, 1);
         }
-        
-        $('.hook').html('');
-        for (let i = 0; i < data.length; i++) {
-            const el = data[i];
 
-            $('.hook').append('<li>' + el + '</li>');
+        
+        currentHook.html('');
+        for (let i = 0; i < currentList.length; i++) {
+            const el = currentList[i];
+
+            currentHook.append('<p>' + el + '</p>');
             
         }
-    })
+    });
+
+    var showList = function(list) {
+
+    }
 
     $('#export').click(function(e){
         $(document).googoose({
